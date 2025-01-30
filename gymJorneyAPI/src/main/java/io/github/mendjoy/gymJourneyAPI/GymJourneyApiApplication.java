1d6759e762c0a8ba5830be0a820846a4074ceb71
mendjoy <joycemendes.jm79@gmail.com>
2025-01-29 21:52:39 -0300

    ajustes e banco h2 para testes

--- gymJorneyAPI/src/main/java/io/github/mendjoy/gymJourneyAPI/GymJourneyApiApplication.java
+++ gymJorneyAPI/src/main/java/io/github/mendjoy/gymJourneyAPI/GymJourneyApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GymJourneyApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GymJourneyApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GymJourneyApiApplication.class, args);
+    }
 
 }
